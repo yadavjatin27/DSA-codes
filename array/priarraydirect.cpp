03c0acd0b06715b581004ee3f96a451b0c7d117b
yadavjatin27 <147433464+yadavjatin27@users.noreply.github.com>
2023-11-22 18:18:38 +0530

    Create priarraydirect.cpp

---
+++ array/priarraydirect.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//printing of array
+int dost[5] = {1,2,3,4,5};
+
+int n=5;
+
+cout<< "printing array" << endl;
+
+for(int i = 0; i<n; i++){
+    cout<< dost[i] << endl;
+}
+
+}
